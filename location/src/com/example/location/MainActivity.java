7e726aedf5808b61e7534604067750f0cce0535b
stan <stan@116.121.158.142>
2014-10-11 00:46:47 +0900

    가속도 Test

--- location/src/com/example/location/MainActivity.java
+++ location/src/com/example/location/MainActivity.java
@@ -1,36 +1,55 @@
 package com.example.location;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
+	
+	SensorManager sm = null;
+	Sensor accSensor = null;
+	SensorEventListener sel = null;
+	
+	MView mView = null;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+				
+		setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+		mView = (MView)findViewById(R.id.mView1);
+		
+		sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+		accSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		sel = new SensorEventListener() {
 
+			@Override
+			public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+			@Override
+			public void onSensorChanged(SensorEvent event)
+			{
+				mView.add(event.values[0], event.values[1], event.values[2], event.timestamp);
+			}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		};
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();		
+		sm.unregisterListener(sel);
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();		
+		sm.registerListener(sel, accSensor, SensorManager.SENSOR_DELAY_GAME);
+	}
 }
